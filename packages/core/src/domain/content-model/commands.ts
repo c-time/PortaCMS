4eae0a67f4a9272bf57b55617d34fc9782a52808
Oka Gentaro <1024975+gentarooka@users.noreply.github.com>
2025-11-22 21:23:59 +0900

    update commands

---
+++ packages/core/src/domain/content-model/commands.ts
@@ -0,0 +1,24 @@
+
+// 
+
+
+
+// create content model command
+
+// delete content model command
+
+// update *** of content model command
+
+// add field of content model command
+
+// remove field
+
+// change order of fields
+
+
+// assign group to a field
+
+
+// add a group
+
+// remove a group
